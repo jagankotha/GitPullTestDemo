4d8452f21a391673cd64198a00972d838c89ffa5
jagankotha <86580836+jagankotha@users.noreply.github.com>
2021-10-06 13:04:11 +0530

    new line added.

--- GitPullTestDemo/src/main/java/com/example/GitPullTestDemo/GitPullTestDemoApplication.java
+++ GitPullTestDemo/src/main/java/com/example/GitPullTestDemo/GitPullTestDemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GitPullTestDemoApplicationTest {
 
 	public static void main(String[] args) {
+		System.out.println("testing purpose");
 		SpringApplication.run(GitPullTestDemoApplication.class, args);
 	}
 
