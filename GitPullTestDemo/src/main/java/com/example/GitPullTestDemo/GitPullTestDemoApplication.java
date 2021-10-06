57b054ddc99671b80a54b4e7187c98bf5aaf9250
jagankotha <86580836+jagankotha@users.noreply.github.com>
2021-10-06 12:43:54 +0530

    ClassName changed.

--- GitPullTestDemo/src/main/java/com/example/GitPullTestDemo/GitPullTestDemoApplication.java
+++ GitPullTestDemo/src/main/java/com/example/GitPullTestDemo/GitPullTestDemoApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GitPullTestDemoApplication {
+public class GitPullTestDemoApplicationTest {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GitPullTestDemoApplication.class, args);
