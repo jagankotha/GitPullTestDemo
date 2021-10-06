6d009acbb2f164cfdd1b78ebf4fe63ecb7c83af4
ram123G <rammohanreddyk19@gmail.com>
2021-10-06 12:32:59 +0530

    inistial commit

---
+++ GitPullTestDemo/src/main/java/com/example/GitPullTestDemo/GitPullTestDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.GitPullTestDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitPullTestDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitPullTestDemoApplication.class, args);
+	}
+
+}
---
+++ GitPullTestDemo/src/test/java/com/example/GitPullTestDemo/GitPullTestDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GitPullTestDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitPullTestDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
